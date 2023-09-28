9830606459a57e1c3c8428e33fbf0ef71b1c3383
ChandanKrv <chandan31jan97@gmail.com>
2023-09-28 11:04:07 +0530

    Need to Improve

--- Week_1/1_Patterns/22_NumStarSoildHalfDiamond2.cpp
+++ Week_1/1_Patterns/22_NumStarSoildHalfDiamond2.cpp
@@ -5,7 +5,7 @@ Numeric Star and Number Mixed Half Diamond
 
 */
 int main()
-{
+{//Need to Imporve the logic, Not working for n=5
     int n = 4, count = 1;
     for (int i = 1; i <= n; i++)
     {
@@ -21,12 +21,10 @@ int main()
         }
         cout << endl;
     }
-
+    count = (n * 2) - 1;
     for (int i = 1; i <= n; i++)
     {
-
-        count = (n * 2) - (i * 2);
-        for (int j = 1; j <= 2 * (n - i) + 1; j++)
+        for (int j = 1; j <= (2 * n) - (2 * i) + 1; j++)
         {
             if ((j ^ 1) == (j + 1))
                 cout << "*";
@@ -37,6 +35,8 @@ int main()
             }
         }
         cout << endl;
+        count = (count - 2) / 2;
     }
+
     return 0;
 }
