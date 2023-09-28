af413cec0e557f7b7d9632e37117ca4855b477c9
ChandanKrv <chandan31jan97@gmail.com>
2023-09-28 07:52:33 +0530

    Success

--- Week_1/1_Patterns/18_HollowDiamond.cpp
+++ Week_1/1_Patterns/18_HollowDiamond.cpp
@@ -2,11 +2,20 @@
 using namespace std;
 /* PROBLEM:
 Hollow Diamond:
-
+    *
+   * *
+  *   *
+ *     *
+*       *
+*       *
+ *     *
+  *   *
+   * *
+    *
 */
 int main()
 {
-    int n = 6;
+    int n = 5;
     // Upper Stars
     for (int i = 1; i <= n; i++)
     {
@@ -23,11 +32,11 @@ int main()
     // Lower Stars
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= i - 1; j++)
             cout << " ";
 
-        for (int j = 1; j <= n - i; j++)
-            if (j == 1 or j == n - i)
+        for (int j = 1; j <= n - i + 1; j++)
+            if (j == 1 or j == n - i + 1)
                 cout << "* ";
             else
                 cout << "  ";
--- Week_1/1_Patterns/21_NumStarSolidHalfDiamond.cpp
+++ Week_1/1_Patterns/21_NumStarSolidHalfDiamond.cpp
@@ -2,32 +2,45 @@
 using namespace std;
 /* PROBLEM:
 Numeric Star Mixed Soild Half Diamond:
-
+1
+2*2
+3*3*3
+4*4*4*4
+5*5*5*5*5
+5*5*5*5*5
+4*4*4*4
+3*3*3
+2*2
+1
 */
 int main()
 {
-    int n = 6, count = 1;
+    int n = 5, count = 1;
+    // For upper half
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= 2 * i - 1; j++)
         {
             if ((j ^ 1) == (j + 1))
                 cout << "*";
-            cout << count;
+            else
+                cout << count;
         }
         count++;
         cout << endl;
     }
-
+    // For Lower Half
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n - i; j++)
+        count--;
+        for (int j = 1; j <= 2 * (n - i) + 1; j++)
         {
             if ((j ^ 1) == (j + 1))
                 cout << "*";
-            cout << count;
+            else
+                cout << count;
         }
-        count--;
+
         cout << endl;
     }
     return 0;
