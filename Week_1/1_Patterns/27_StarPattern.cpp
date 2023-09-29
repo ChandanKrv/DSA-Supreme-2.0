98b7e24da085c3fdc41f74396019c0f4972adfac
ChandanKrv <chandan31jan97@gmail.com>
2023-09-29 12:01:11 +0530

    initial commit

--- Week_1/1_Patterns/27_StarPattern.cpp
+++ Week_1/1_Patterns/27_StarPattern.cpp
@@ -1,40 +1,21 @@
 #include <iostream> // Created by @ChandanKrV on 29 Sep, 2023.
 using namespace std;
 /* PROBLEM:
-**********
-****  ****
-***    ***
-**      **
-*        *
-*        *
-**      **
-***    ***
-****  ****
-**********
-*/
+    *        *
+    **      **
+    ***    ***
+    ****  ****
+    **********
+    **********
+    ****  ****
+    ***    ***
+    **      **
+    *        *
+ */
 int main()
 {
     int n = 5;
-    // For Upper Half
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n - i + 1; j++)
-        {
-            cout << "*";
-        }
-        // For middle spaces
-        for (int j = 1; j <= i - 1; j++)
-        {
-            cout << "  ";
-        }
-
-        for (int j = 1; j <= n - i + 1; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-    // For lower half
+    // For upper half
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
@@ -48,6 +29,25 @@ int main()
         }
 
         for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    // For Lower Half
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "*";
+        }
+        // For middle spaces
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout << "  ";
+        }
+
+        for (int j = 1; j <= n - i + 1; j++)
         {
             cout << "*";
         }
