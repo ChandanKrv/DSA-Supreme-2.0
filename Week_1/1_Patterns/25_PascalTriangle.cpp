e8031b4ea6c675aad5e7d69e3b0daa0f632926a0
ChandanKrv <chandan31jan97@gmail.com>
2023-10-06 13:28:49 +0530

    Pattern completed

--- Week_1/1_Patterns/25_PascalTriangle.cpp
+++ Week_1/1_Patterns/25_PascalTriangle.cpp
@@ -1,19 +1,26 @@
 #include <iostream> // Created by @ChandanKrV on 29 Sep, 2023.
 using namespace std;
 /* PROBLEM:
-Pascal’s Triangle
+Pascal’s Triangle :
+Using Binomial Coefficient : c = c * (i - j) / j;
+Note: i and j index must be start from 1, not from 0
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+1 5 10 10 5 1
 */
 int main()
-{ // Not done
+{
     int n = 6;
     for (int i = 1; i <= n; i++)
     {
+        int c = 1;
         for (int j = 1; j <= i; j++)
         {
-            if (j == 1 or j == i)
-                cout << 1;
-            else
-                cout << "*";
+            cout << c << " ";
+            c = c * (i - j) / j;
         }
         cout << endl;
     }
