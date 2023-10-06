33773441a2dbf49bf47941129ce8c317330ff43e
ChandanKrv <chandan31jan97@gmail.com>
2023-10-06 13:32:05 +0530

    Pattern completed

--- Week_1/1_Patterns/22_NumStarSoildHalfDiamond2.cpp
+++ Week_1/1_Patterns/22_NumStarSoildHalfDiamond2.cpp
@@ -13,9 +13,7 @@ Enter the no. of rows: 4
 */
 int main()
 {
-    int n, c = 1;
-    cout << "Enter the no. of rows: ";
-    cin >> n;
+    int n = 4, c = 1;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j <= i; j++)
@@ -27,7 +25,7 @@ int main()
         }
         cout << endl;
     }
-    // Shrinking phase
+    // Shrinking Phase
     int start = c - n;
     for (int i = 0; i < n; i++)
     {
@@ -44,47 +42,3 @@ int main()
     }
     return 0;
 }
-
-/* #include <iostream> // Created by @ChandanKrV on 28 Sep, 2023.
-using namespace std;
-/* PROBLEM:
-Numeric Star and Number Mixed Half Diamond
-
-*/
-/*
-int main()
-{//Need to Imporve the logic, Not working for n=5
-    int n = 4, count = 1;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= 2 * i - 1; j++)
-        {
-            if ((j ^ 1) == (j + 1))
-                cout << "*";
-            else
-            {
-                cout << count;
-                count++;
-            }
-        }
-        cout << endl;
-    }
-    count = (n * 2) - 1;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= (2 * n) - (2 * i) + 1; j++)
-        {
-            if ((j ^ 1) == (j + 1))
-                cout << "*";
-            else
-            {
-                cout << count;
-                count++;
-            }
-        }
-        cout << endl;
-        count = (count - 2) / 2;
-    }
-
-    return 0;
-} */
