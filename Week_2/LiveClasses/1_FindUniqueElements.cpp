fbea14cdd1990c9ded7555cabd25c0ff859c7618
ChandanKrv <chandan31jan97@gmail.com>
2023-10-08 17:40:42 +0530

    initial commit

--- Week_2/LiveClasses/1_FindUniqueElements.cpp
+++ Week_2/LiveClasses/1_FindUniqueElements.cpp
@@ -5,9 +5,15 @@ Print unique elements of the given array.
 */
 int main()
 {
+    int arr[] = {10, 2, 11, 10, 2, 13, 15, 13, 15};
+    int n = sizeof(arr) / sizeof(int);
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        ans = ans ^ arr[i];
 
+    cout << "Enqiue element is: " << ans;
     return 0;
 }
 /* OUTPUT
-
+Enqiue element is: 11
 */
