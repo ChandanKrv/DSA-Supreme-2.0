dd312106b5494ca2fad790d46dd234ae34a2a9fb
ChandanKrv <chandan.nit.cse@gmail.com>
2024-05-06 22:59:18 +0530

    initial commit

--- Week_2/LiveClasses/7_SortColors.cpp
+++ Week_2/LiveClasses/7_SortColors.cpp
@@ -1,7 +1,23 @@
 #include <iostream> // Created by @ChandanKrV on 11 Oct, 2023.
 using namespace std;
 /* PROBLEM:
-Leecode 75:
+Leecode 75: Sort Colors
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
 */
 int main()
 {
@@ -39,5 +55,5 @@ int main()
     return 0;
 }
 /* OUTPUT
-0 0 1 1 2 2 
+0 0 1 1 2 2
 */
